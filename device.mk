9ba4a7e789f302babd0a433fa73a84c29993b8c1
somadsul <somnathadsul@gmail.com>
2016-12-01 12:55:13 +0530

    Device.mk Decomment Camera prop & remove kernel headers

--- device.mk
+++ device.mk
@@ -264,8 +264,8 @@ PRODUCT_PACKAGES += \
 	libhwjpeg
 
 # This fixes switching between front/back camera sensors
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    camera2.portability.force_api=1
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
 
 ###########################################################
 ### TOUCHSCREEN/KEYLAYOUTS
